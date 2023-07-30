a6dbb5344d29a02a932fffbaf590b4832925b219
erick molina <eralejo2003@gmail.com>
2023-07-29 18:18:21 -0600

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './componets/sign-up/sign-up.component';
-import { LoginComponent } from './componets/login/login.component';
-import { ProfileComponent } from './componets/profile/profile.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({

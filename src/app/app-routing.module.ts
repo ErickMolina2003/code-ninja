4fe40d3a0546df4dc3928c2f005d3923a89f11c5
erick molina <eralejo2003@gmail.com>
2023-07-29 20:15:01 -0600

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {
-    path: 'sidebar',
-    component: SidebarComponent,
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: LandingPageComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'signup',
+        component: SignUpComponent,
+      },
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
+    ],
   },
 ];
 
